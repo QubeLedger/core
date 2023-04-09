98854da20e766e196600edf05f5dbbba91a271e4
knstntn <konstantinjogotsen@gmail.com>
2023-04-09 22:16:12 +0500

    fix(x/printer/client): change number of arguments of Burn function
    * in tx_burn.go "cobra.ExactArgs(1)" -> "cobra.ExactArgs(2)"

--- x/printer/client/cli/tx_burn.go
+++ x/printer/client/cli/tx_burn.go
@@ -17,7 +17,7 @@ func CmdBurn() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "burn [amount, validator]",
 		Short: "USQ combustion to obtain Qube",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(2),
 		Long:  strings.TrimSpace(`$ quadrated printer burn 10000usq qubevaloperkgb366x4hqa4d3457cjjizwsnhp9m4hhw7fby72`),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			//argAmount := args[0]
