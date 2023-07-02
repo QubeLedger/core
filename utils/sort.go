555d17c248e90911e36fcdd7c45c920020a414ee
knstntn <konstantin@quadrate.org>
2023-07-03 01:18:19 +0500

    fix: add #nosec
    * (fix unknown err)
    
    Co-authored-by: Konstantin Kljuchnikov <konstantin@quadrate.org>

--- utils/sort.go
+++ utils/sort.go
@@ -1,4 +1,3 @@
-/* #nosec */
 package utils
 
 import (
@@ -8,6 +7,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+/* #nosec */
 func Keys[V interface{}](in map[string]V) []string {
 	out := make([]string, 0)
 
@@ -22,12 +22,14 @@ func Keys[V interface{}](in map[string]V) []string {
 
 // SortSlice sorts a slice of type T elements that implement constraints.Ordered.
 // Mutates input slice s.
+/* #nosec */
 func SortSlice[T constraints.Ordered](s []T) {
 	sort.Slice(s, func(i, j int) bool {
 		return s[i] < s[j]
 	})
 }
 
+/* #nosec */
 func Unique[V interface{}](in []V) []V {
 	keys := make(map[string]struct{}, len(in))
 	list := []V{}
