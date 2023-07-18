cf9e8a049e3d9785e0cbb359b3dfeb27244fb40a
knstntn <konstantin@quadrate.org>
2023-07-18 21:52:27 +0300

    test(x/stable): Add new tests
    * refactor old tests to work with the previous commit
    
    * added new tests for working with oracle
    
    Co-authored-by: Konstantin Kljuchnikov <konstantin@quadrate.org>

--- x/stable/keeper/msg_server_test.go
+++ x/stable/keeper/msg_server_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/QuadrateOrg/core/x/stable/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -51,11 +52,12 @@ func (suite *StableKeeperTestSuite) TestMintUsq() {
 	}
 
 	for _, tc := range testCases {
+		suite.Setup()
+		suite.Commit()
+		suite.app.StableKeeper.SetTestingMode(true)
 		suite.Run(fmt.Sprintf("Case    %s", tc.name), func() {
-			suite.Setup()
-			suite.Commit()
 			suite.app.StableKeeper.SetBaseTokenDenom(suite.ctx, tc.baseTokenDenom)
-			suite.app.StableKeeper.SetAtomPriceForTest(suite.ctx, sdk.NewInt(tc.atomPrice))
+			suite.app.StableKeeper.UpdateAtomPriceTesting(suite.ctx, sdk.NewInt(tc.atomPrice))
 
 			suite.app.BankKeeper.MintCoins(suite.ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(tc.atomAmount))))
 			suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, types.ModuleName, suite.Address, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(tc.atomAmount))))
@@ -123,16 +125,22 @@ func (suite *StableKeeperTestSuite) TestBurnUsq() {
 	for _, tc := range testCases {
 		suite.Setup()
 		suite.Commit()
+
+		suite.app.StableKeeper.SetTestingMode(true)
+
 		suite.app.BankKeeper.MintCoins(suite.ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(10000))))
 		suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, types.ModuleName, suite.Address, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(10000))))
+
 		atomAmount := suite.app.BankKeeper.GetBalance(suite.ctx, suite.Address, "uatom")
 		suite.Require().Equal(atomAmount.Amount, sdk.NewInt(10000))
 		suite.Commit()
+
 		err := suite.MintUsq()
 		suite.Require().NoError(err)
+
 		suite.Run(fmt.Sprintf("Case    %s", tc.name), func() {
 			suite.app.StableKeeper.SetBaseTokenDenom(suite.ctx, tc.baseTokenDenom)
-			suite.app.StableKeeper.SetAtomPriceForTest(suite.ctx, sdk.NewInt(tc.atomPrice))
+			suite.app.StableKeeper.UpdateAtomPriceTesting(suite.ctx, sdk.NewInt(tc.atomPrice))
 
 			msg := types.NewMsgBurnUsq(
 				suite.Address.String(),
@@ -154,7 +162,7 @@ func (suite *StableKeeperTestSuite) TestBurnUsq() {
 
 func (suite *StableKeeperTestSuite) MintUsq() error {
 	suite.app.StableKeeper.SetBaseTokenDenom(suite.ctx, "uatom")
-	suite.app.StableKeeper.SetAtomPriceForTest(suite.ctx, sdk.NewInt(95000))
+	suite.app.StableKeeper.UpdateAtomPriceTesting(suite.ctx, sdk.NewInt(95000))
 	msg := types.NewMsgMintUsq(
 		suite.Address.String(),
 		sdk.NewInt(10000).String()+"uatom",
@@ -165,4 +173,122 @@ func (suite *StableKeeperTestSuite) MintUsq() error {
 		return err
 	}
 	return nil
+}
+
+func (suite *StableKeeperTestSuite) TestMintUsqGetPriceFromOracle() {
+	testCases := []struct {
+		name           string
+		baseTokenDenom string
+		sendTokenDenom string
+		atomAmount     int64
+		err            bool
+		errString      string
+	}{
+		{
+			"ok - mint",
+			"uatom",
+			"uatom",
+			1000,
+			false,
+			"",
+		},
+		{
+			"ok - mint",
+			"uatom",
+			"uatom",
+			300,
+			false,
+			"",
+		},
+		{
+			"ok - mint",
+			"uatom",
+			"uatom",
+			730,
+			false,
+			"",
+		},
+	}
+	suite.Setup()
+	suite.Commit()
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.app.BankKeeper.MintCoins(suite.ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(10000))))
+			suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, types.ModuleName, suite.Address, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(10000))))
+			suite.app.StableKeeper.SetBaseTokenDenom(suite.ctx, tc.baseTokenDenom)
+			msg := types.NewMsgMintUsq(
+				suite.Address.String(),
+				sdk.NewInt(tc.atomAmount).String()+tc.sendTokenDenom,
+			)
+			ctx := sdk.WrapSDKContext(suite.ctx)
+			_, err := suite.app.StableKeeper.MintUsq(ctx, msg)
+			suite.Require().NoError(err)
+			uusdSuply := suite.app.BankKeeper.GetBalance(suite.ctx, suite.Address, "uusd")
+			suite.Require().Greater(uusdSuply.Amount.Int64(), int64(0))
+		})
+	}
+}
+
+func (suite *StableKeeperTestSuite) TestBurnUsqGetPriceFromOracle() {
+	testCases := []struct {
+		name           string
+		baseTokenDenom string
+		sendTokenDenom string
+		uusdAmount     int64
+		err            bool
+		errString      string
+	}{
+		{
+			"ok - burn",
+			"uatom",
+			"uusd",
+			1000,
+			false,
+			"",
+		},
+		{
+			"ok - burn",
+			"uatom",
+			"uusd",
+			300,
+			false,
+			"",
+		},
+		{
+			"ok - burn",
+			"uatom",
+			"uusd",
+			730,
+			false,
+			"",
+		},
+	}
+	suite.Setup()
+	suite.Commit()
+
+	suite.app.BankKeeper.MintCoins(suite.ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(10000))))
+	suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, types.ModuleName, suite.Address, sdk.NewCoins(sdk.NewCoin("uatom", sdk.NewInt(10000))))
+
+	atomAmount := suite.app.BankKeeper.GetBalance(suite.ctx, suite.Address, "uatom")
+	suite.Require().Equal(atomAmount.Amount, sdk.NewInt(10000))
+	suite.Commit()
+
+	err := suite.MintUsq()
+	suite.Require().NoError(err)
+
+	suite.app.StableKeeper.SetBaseTokenDenom(suite.ctx, suite.app.StableKeeper.GetBaseTokenDenom(suite.ctx))
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			msg := types.NewMsgBurnUsq(
+				suite.Address.String(),
+				sdk.NewInt(tc.uusdAmount).String()+tc.sendTokenDenom,
+			)
+			ctx := sdk.WrapSDKContext(suite.ctx)
+			_, err := suite.app.StableKeeper.BurnUsq(ctx, msg)
+			suite.Require().NoError(err)
+			uatomSuply := suite.app.BankKeeper.GetBalance(suite.ctx, suite.Address, "uatom")
+			fmt.Printf("uatomSuply: %s \n", uatomSuply)
+			suite.Require().Greater(uatomSuply.Amount.Int64(), int64(0))
+		})
+	}
 }
