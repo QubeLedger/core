d37ca189287571e944fb0b4720138ca761b92ab7
knstntn <konstantin@quadrate.org>
2023-10-05 21:44:48 +0500

    fix: update price for testnet

--- x/stable/keeper/price.go
+++ x/stable/keeper/price.go
@@ -1,7 +1,13 @@
 package keeper
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
 	"github.com/QuadrateOrg/core/x/stable/types"
+	"github.com/buger/jsonparser"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -17,7 +23,7 @@ func (k Keeper) UpdateAtomPrice(ctx sdk.Context, pair types.Pair) error {
 	if AtomPrice.IsNil() {
 		AtomPrice = sdk.NewInt(0)
 	}
-	price, err := k.oracleKeeper.GetExchangeRate(ctx, pair.AmountInMetadata.Base)
+	price, err := GetPrice() //k.oracleKeeper.GetExchangeRate(ctx, pair.AmountInMetadata.Base)
 	if err != nil {
 		return err
 	}
@@ -26,6 +32,33 @@ func (k Keeper) UpdateAtomPrice(ctx sdk.Context, pair types.Pair) error {
 	}
 	AtomPrice = price.MulInt64(10000).RoundInt()
 	return nil
+}
+
+func GetPrice() (sdk.Dec, error) {
+	var atomPriceString string
+
+	tr := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    30 * time.Second,
+		DisableCompression: true,
+	}
+	client := &http.Client{Transport: tr}
+
+	res, err := client.Get("https://api.coinbase.com/v2/exchange-rates?currency=ATOM")
+	if err != nil {
+		return sdk.ZeroDec(), err
+	}
+	body, _ := ioutil.ReadAll(res.Body)
+
+	if value, err := jsonparser.GetString(body, "data", "rates", "USD"); err == nil {
+		atomPriceString = fmt.Sprintf("%v", value)
+	} else {
+		return sdk.ZeroDec(), err
+	}
+
+	val, err := sdk.NewDecFromStr(atomPriceString)
+
+	return val, nil
 }
 
 func (k Keeper) UpdateAtomPriceTesting(ctx sdk.Context, price sdk.Int) error {
