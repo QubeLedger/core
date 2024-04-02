b8e816b6937035896a631530f5f729d4760f0982
knstntn <konstantin@quadrate.org>
2024-04-03 01:04:19 +0500

    feat(x/perpetual): add msg to codec
    Co-authored-by: Konstantin <konstantin@quadrate.org>

--- x/perpetual/types/codec.go
+++ x/perpetual/types/codec.go
@@ -3,16 +3,30 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
-	// this line is used by starport scaffolding # 1
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	// this line is used by starport scaffolding # 2
+	cdc.RegisterConcrete(&MsgPerpetualDeposit{}, "perpetual/PerpetualDeposit", nil)
+	cdc.RegisterConcrete(&MsgPerpetualWithdraw{}, "perpetual/PerpetualWithdraw", nil)
+	cdc.RegisterConcrete(&MsgCreatePosition{}, "perpetual/CreatePosition", nil)
+	cdc.RegisterConcrete(&MsgClosePosition{}, "perpetual/ClosePosition", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	// this line is used by starport scaffolding # 3
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgPerpetualDeposit{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgPerpetualWithdraw{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCreatePosition{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgClosePosition{},
+	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
